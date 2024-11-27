f6c0ee49234a125c9d9d35fc1c1b0277515320a7
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2024-11-27 20:57:38 +0800

    add 2 test cases

--- examples/ram-assumable-role/main.tf
+++ examples/ram-assumable-role/main.tf
@@ -145,3 +145,54 @@ module "ram_assumable_role_with_custom_trust_policy" {
     "AliyunECSReadOnlyAccess"
   ]
 }
+
+###############################
+# RAM assumable role with trusted service
+###############################
+module "ram_assumable_role_with_trusted_service" {
+  source = "../../modules/ram-assumable-role"
+
+  create_role = true
+  role_name   = "role-with-trusted-service"
+  description = "test RAM assumable role with trusted service"
+
+  action                 = "sts:AssumeRole"
+
+  trusted_role_services = [
+    "ecs.aliyuncs.com"
+  ]
+}
+
+###############################
+# RAM assumable role with array of action
+###############################
+# # TODO: resolve test failure - RAM assumable role with array of action
+# module "ram_assumable_role_with_array_of_action" {
+#   source = "../../modules/ram-assumable-role"
+
+#   create_role = true
+#   role_name   = "role-with-array-of-action"
+#   description = "test RAM assumable role with array of action"
+
+#   create_custom_role_trust_policy = true
+#   custom_role_trust_policy = <<EOF
+#   {
+#     "Statement": [
+#       {
+#         "Action": [
+#             "sts:AssumeRole"
+#         ],
+#         "Effect": "Allow",
+#         "Principal": {
+#             "Service": "fc.aliyuncs.com"
+#         }
+#       }
+#     ],
+#       "Version": "1"
+#   }
+#   EOF
+
+#   trusted_role_arns = [
+#     "acs:ram::${data.alicloud_account.this.id}:root"
+#   ]
+# }
