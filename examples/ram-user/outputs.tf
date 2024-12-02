969fb7e33fe140d6c094ea72020069fd82504cab
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2024-12-02 17:30:10 +0800

    (3.12) add iam_access_key_secret iam_access_key_encrypted_secret iam_access_key_key_fingerprint variables in ram-user module

--- examples/ram-user/outputs.tf
+++ examples/ram-user/outputs.tf
@@ -8,4 +8,23 @@ output "this_ram_user_unique_id" {
   value       = module.ram_user.this_ram_user_unique_id
 }
 
+output "this_ram_access_key_id" {
+  description = "The unique ID assigned by Alibaba Cloud"
+  value       = module.ram_user_with_encrypted_secret.this_ram_access_key_id
+}
 
+output "this_ram_access_key_secret" {
+  description = "The access key secret"
+  value       = module.ram_user_with_encrypted_secret.this_ram_access_key_secret
+  sensitive   = true
+}
+
+output "this_ram_access_key_encrypted_secret" {
+  description = "The access key encrypted secret, base64 encoded"
+  value       = module.ram_user_with_encrypted_secret.this_ram_access_key_encrypted_secret
+}
+
+output "this_ram_access_key_key_fingerprint" {
+  description = "The fingerprint of the PGP key used to encrypt the secret"
+  value       = module.ram_user_with_encrypted_secret.this_ram_access_key_key_fingerprint
+}
--- modules/ram-user/outputs.tf
+++ modules/ram-user/outputs.tf
@@ -12,3 +12,19 @@ output "this_ram_access_key_id" {
   description = "The access key ID"
   value       = element(concat(alicloud_ram_access_key.this.*.id, [""]), 0)
 }
+
+output "this_ram_access_key_secret" {
+  description = "The access key secret"
+  value       = element(concat(alicloud_ram_access_key.this.*.secret, [""]), 0)
+  sensitive   = true
+}
+
+output "this_ram_access_key_encrypted_secret" {
+  description = "The access key encrypted secret, base64 encoded"
+  value       = element(concat(alicloud_ram_access_key.this.*.encrypted_secret, [""]), 0)
+}
+
+output "this_ram_access_key_key_fingerprint" {
+  description = "The fingerprint of the PGP key used to encrypt the secret"
+  value       = element(concat(alicloud_ram_access_key.this.*.key_fingerprint, [""]), 0)
+}
