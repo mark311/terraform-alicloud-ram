57ae51028268dd4e723b14483b9a9713f1681a0c
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2024-12-12 14:01:17 +0800

    add output this_caller_identity_arn and this_caller_identity_principal_id in module ram-account

--- examples/ram-account/outputs.tf
+++ examples/ram-account/outputs.tf
@@ -1,3 +1,18 @@
+output "this_caller_identity_account_id" {
+  description = "The Alicloud Account ID number of the account that owns or contains the calling entity"
+  value       = module.ram_account.this_caller_identity_account_id
+}
+
+output "this_caller_identity_arn" {
+  description = "The ARN of the calling identity"
+  value       = module.ram_account.this_caller_identity_arn
+}
+
+output "this_caller_identity_principal_id" {
+  description = "The principal ID of the calling identity"
+  value       = module.ram_account.this_caller_identity_principal_id
+}
+
 output "this_ram_account_alias" {
   description = "Alias of cloud account."
   value       = module.ram_account.this_ram_account_alias
--- modules/ram-account/outputs.tf
+++ modules/ram-account/outputs.tf
@@ -3,6 +3,16 @@ output "this_caller_identity_account_id" {
   value       = element(concat(data.alicloud_caller_identity.this.*.id, [""]), 0)
 }
 
+output "this_caller_identity_arn" {
+  description = "The ARN of the calling identity"
+  value       = element(concat(data.alicloud_caller_identity.this.*.arn, [""]), 0)
+}
+
+output "this_caller_identity_principal_id" {
+  description = "The principal ID of the calling identity"
+  value       = element(concat(data.alicloud_caller_identity.this.*.id, [""]), 0)
+}
+
 output "this_ram_account_alias" {
   description = "Alias of alicloud account."
   value       = concat(alicloud_ram_account_alias.this.*.account_alias, [""])[0]
