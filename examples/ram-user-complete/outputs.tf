2f4aef554866c7cfaaa3e7c5b8ce985d3ff71367
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2024-12-20 18:06:10 +0800

    add example ram-user-complete

---
+++ examples/ram-user-complete/outputs.tf
@@ -0,0 +1,40 @@
+output "this_ram_user_name" {
+  description = "The user's name"
+  value       = module.ram-user-example.this_ram_user_name
+}
+
+output "this_ram_user_unique_id" {
+  description = "The unique ID assigned by Alibaba Cloud"
+  value       = module.ram-user-example.this_ram_user_unique_id
+}
+
+output "this_ram_access_key_id" {
+  description = "The unique ID assigned by Alibaba Cloud"
+  value       = module.ram-user-example.this_ram_access_key_id
+}
+
+output "this_ram_access_key_secret" {
+  description = "The access key secret"
+  value       = module.ram-user-example.this_ram_access_key_secret
+  sensitive   = true
+}
+
+output "this_ram_access_key_encrypted_secret" {
+  description = "The access key encrypted secret, base64 encoded"
+  value       = module.ram-user-example.this_ram_access_key_encrypted_secret
+}
+
+output "this_ram_access_key_key_fingerprint" {
+  description = "The fingerprint of the PGP key used to encrypt the secret"
+  value       = module.ram-user-example.this_ram_access_key_key_fingerprint
+}
+
+output "this_ram_access_key_status" {
+  description = "Active or Inactive. Keys are initially active, but can be made inactive by other means."
+  value       = module.ram-user-example.this_ram_access_key_status
+}
+
+output "this_pgp_key" {
+  description = "PGP key used to encrypt sensitive data for this user (if empty, no encryption)"
+  value       = module.ram-user-example.this_pgp_key
+}
