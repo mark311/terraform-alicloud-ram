abdae85526c78f69f452a72e965500f13622c03a
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2024-12-20 17:10:49 +0800

    add example ram-assumable-role-custom-trust-policy

---
+++ examples/ram-assumable-role-custom-trust-policy/main.tf
@@ -0,0 +1,27 @@
+module "ram-assumable-role-example" {
+  source = "../../modules/ram-assumable-role"
+
+  create_role = true
+  role_name   = "tf-ram-assumable-role-example-custom-trust-policy"
+  description = "tf-ram-assumable-role-example-custom-trust-policy"
+
+  role_requires_mfa         = false
+  attach_admin_policy       = true
+
+  create_custom_role_trust_policy = true
+  custom_role_trust_policy = <<EOF
+  {
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "fc.aliyuncs.com"
+        }
+      }
+    ],
+      "Version": "1"
+  }
+  EOF
+
+}
